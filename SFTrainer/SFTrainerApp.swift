4b9deec6b7bdf30480c1d55d3cb7440aae7973d9
antoniovirgone <antonaccio86@gmail.com>
2025-11-23 22:38:23 +0100

    Initial Commit

---
+++ SFTrainer/SFTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFTrainerApp.swift
+//  SFTrainer
+//
+//  Created by Antonio Virgone on 23/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SFTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
